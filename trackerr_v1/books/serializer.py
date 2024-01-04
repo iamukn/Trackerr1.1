8e1e256512f230eb5f4a6491c815305b02e79817
iamukn <n.u.kingsley@gmail.com>
2024-01-04 19:42:51 +0000

    updated the serializers depth class variable to 1 to allow for proper relationship data display

--- trackerr_v1/books/serializer.py
+++ trackerr_v1/books/serializer.py
@@ -6,7 +6,7 @@ class BooksSerializer(ModelSerializer):
     class Meta:
         model = Books
         fields = ['id', 'title', 'isbn', 'chapter', 'relationship']
-     #   depth = 1
+        depth = 1
 
 class AuthorSerializer(ModelSerializer):
     class Meta:
