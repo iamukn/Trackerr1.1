22fe63fbdeaf600bfd88c0b1ed574769ee578ebe
iamukn <n.u.kingsley@gmail.com>
2024-01-08 16:18:02 +0000

    tested the query_params method to receive parameters from a function

--- trackerr_v1/drf/views.py
+++ trackerr_v1/drf/views.py
@@ -1,3 +1,15 @@
+from rest_framework.response import Response
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+
+
+
+@api_view(['GET'])
+def home(request):
+    print(request.query_params.dict())
+    return Response('Hello World')
 
 # Create your views here.
+
+
