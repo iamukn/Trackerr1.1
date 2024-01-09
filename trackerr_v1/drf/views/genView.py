2e07dadb2fbbfc7db860042348484733a1d138c3
iamukn <n.u.kingsley@gmail.com>
2024-01-09 22:00:39 +0000

    overriding the list method inside the ListAPIView class

--- trackerr_v1/drf/views/genView.py
+++ trackerr_v1/drf/views/genView.py
@@ -2,8 +2,16 @@ from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
 from drf.serializer import UsersSerializer
+from rest_framework.response import Response
 
 class GenView(generics.ListAPIView):
     permission_classes = [AllowAny]
     queryset = User.objects.all()
     serializer_class = UsersSerializer
+
+    def list(self, request):
+
+        query = self.get_queryset()
+
+        serializer = UsersSerializer(query, many=True)
+        return Response(serializer.data)
