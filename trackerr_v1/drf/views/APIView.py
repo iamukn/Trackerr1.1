eaf9ef55b0aef797819c9e6e1824f9446c1ed85e
iamukn <n.u.kingsley@gmail.com>
2024-01-09 21:19:07 +0000

    http_method_allowed controls the http methods that can trigger a view

--- trackerr_v1/drf/views/APIView.py
+++ trackerr_v1/drf/views/APIView.py
@@ -6,6 +6,7 @@ from rest_framework import authentication, permissions
 class ListView(APIView):
     """ This view will list all users in the system """
     permission_classes = [permissions.AllowAny]
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete', 'head', 'options', 'trace']
 
     def get(self, request, format=None, *args, **kwargs):
         user = [(users.username, users.email) for users in User.objects.all()]
