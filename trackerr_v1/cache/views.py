7854dc37a414a127fa4cd2eadb4373897e6638df
iamukn <n.u.kingsley@gmail.com>
2024-01-25 19:24:41 +0000

    added the cache app and its view

---
+++ trackerr_v1/cache/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie, vary_on_headers
+from random import randint
+
+class PostView(APIView):
+    # Cache page for the requested url
+    @method_decorator(cache_page(60 * 60 * 2))
+    def get(self, request, format=None):
+        data = ['Hello', 'World']
+        content = {
+            "title": randint(0, 1),
+            "body": "Post content",
+        }
+        return Response(content)
