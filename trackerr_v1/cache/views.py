4f658deaaf1418565f1a27a893020c44a0d6ff28
iamukn <n.u.kingsley@gmail.com>
2024-01-25 20:23:26 +0000

    throttled the rest for both anonymous users and auth users

--- trackerr_v1/cache/views.py
+++ trackerr_v1/cache/views.py
@@ -7,13 +7,15 @@ from django.views.decorators.cache import cache_page
 from django.views.decorators.vary import vary_on_cookie, vary_on_headers
 from random import randint
 
+from .models import Phone
+from .serializer import PhoneSerializer
+
 class PostView(APIView):
     # Cache page for the requested url
     @method_decorator(cache_page(60 * 60 * 2))
     def get(self, request, format=None):
-        name = ['dan', 'david']
-        content = {
-            "title": data[randint(0, 1)],
-            "body": "Post content",
-        }
-        return Response(content)
+        queryset = Phone.objects.all()
+        ser = PhoneSerializer(queryset, many=True)
+        data = [phones for phones in ser.data]
+
+        return Response(data)
