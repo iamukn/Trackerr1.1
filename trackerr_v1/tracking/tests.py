a72483cf731e60f49debd87dfa963b9bda5bfc00
iamukn <n.u.kingsley@gmail.com>
2024-02-01 20:07:06 +0000

    testing the DefaultRouter() endpoint

--- trackerr_v1/tracking/tests.py
+++ trackerr_v1/tracking/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase, URLPatternsTestCase, APIClient
+from .models import Tracking
+from django.urls import reverse, reverse_lazy
 
-# Create your tests here.
+class Tester(APITestCase):
+
+    def setUp(self):
+        self.data = Tracking.objects.all()
+        for i in range(12):
+            self.data.create(tracking_num=[f'Tester {i}'])
+        return super().setUp()    
+        
+    def TearDown(self):
+        return super().tearDown()
+
+class Testers(Tester):
+
+    def test_get(self):
+        url = '/tracking/1/'
+        data = self.client.get(url) 
+        self.assertEqual(data.status_code, 200)
+
+
