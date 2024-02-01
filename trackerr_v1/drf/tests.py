d02b04303cf5ee06a07484d2eefa914f81d7f218
iamukn <n.u.kingsley@gmail.com>
2024-02-01 21:34:53 +0000

    patch request testing

--- trackerr_v1/drf/tests.py
+++ trackerr_v1/drf/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from .models import Books
+
+class TestBook(APITestCase):
+    
+
+    def test_patch(self):
+        data = Books.objects.create(author='Daniel', message='Hello there')
+        update_data = {"message": "BabaNla", "author": "Ikpa"}
+        req = self.client.patch(f'/drf/myviewset/{data.id}/', update_data, format='json')
+
+        print(req)
+
+        self.assertTrue(req.status_code <= 300)
+
 
 # Create your tests here.
