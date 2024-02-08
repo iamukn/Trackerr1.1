a2604fa170117d2991d28898040ce79b2785abb4
iamukn <n.u.kingsley@gmail.com>
2024-02-08 18:51:59 +0000

    added the species view

--- trackerr_v1/oneToOne/views.py
+++ trackerr_v1/oneToOne/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
-from .serializer import (DepartmentSerializer, EmployeeSerializer)
-from .models import Employee, Department
+from .serializer import (DepartmentSerializer, EmployeeSerializer, SpeciesSerializer)
+from .models import Employee, Department, Species
 
 class Dept_Reg(APIView):
     permission_classes = [AllowAny]
@@ -13,3 +13,12 @@ class Dept_Reg(APIView):
         serializer = EmployeeSerializer(employee, many=True)
         return Response(serializer.data, status='200')
 
+
+class Species_get(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        species = Species.objects.all()
+        serializer = SpeciesSerializer(species, many=True)
+
+        return Response(serializer.data, status='200')
