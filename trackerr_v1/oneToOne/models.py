ff8b08819d6bfcd68d74a65dada099ebed5816e4
iamukn <n.u.kingsley@gmail.com>
2024-02-08 17:30:51 +0000

    created the department and employee model linked using the oneToOne relationship

--- trackerr_v1/oneToOne/models.py
+++ trackerr_v1/oneToOne/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+
+        return self.name
+
+class Employee(models.Model):
+    name = models.CharField(max_length=35, null=False, blank=False)
+    age = models.IntegerField(default=0)
+    department = models.OneToOneField(Department, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+
