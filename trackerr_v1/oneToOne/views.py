8448d13f532318316c4c462b2103cb1e6490da53
iamukn <n.u.kingsley@gmail.com>
2024-02-08 18:11:37 +0000

    Added the Department class import to the view

--- trackerr_v1/oneToOne/views.py
+++ trackerr_v1/oneToOne/views.py
@@ -3,13 +3,13 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from .serializer import (DepartmentSerializer, EmployeeSerializer)
-from .models import Employee
+from .models import Employee, Department
 
 class Dept_Reg(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request):
-        employees = Employee.objects.all()
-        serializer = EmployeeSerializer(employees, many=True)
+        department = Department.objects.all()
+        serializer = DepartmentSerializer(employees, many=True)
         return Response(serializer.data, status='200')
 
