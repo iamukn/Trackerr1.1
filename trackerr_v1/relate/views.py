c1357afbd87e4c66fb75cb4a99f365d13c782bb1
iamukn <n.u.kingsley@gmail.com>
2024-02-10 14:12:56 +0000

    updated the view

--- trackerr_v1/relate/views.py
+++ trackerr_v1/relate/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from .models import Tracking
