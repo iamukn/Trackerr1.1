8e89e1c20f31f1bdc12d93f778ea288fd124d759
iamukn <n.u.kingsley@gmail.com>
2024-02-10 13:59:07 +0000

    added a home view

--- trackerr_v1/relate/views.py
+++ trackerr_v1/relate/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+class Home(APIView):
+    permission_classes = [AllowAny]
+    
+    def get(self, request):
+        return HttpResponse("Welcome to Home")
+
