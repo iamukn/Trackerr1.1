fdaed4a623d7565120b76d9d17f70b460c231aa5
iamukn <n.u.kingsley@gmail.com>
2024-02-10 14:11:28 +0000

    updated the views

--- trackerr_v1/relate/views.py
+++ trackerr_v1/relate/views.py
@@ -3,10 +3,14 @@ from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from .models import Tracking
+from .serializer import TrackingSerializer
 
 class Home(APIView):
     permission_classes = [AllowAny]
     
     def get(self, request):
-        return HttpResponse("Welcome to Home")
+        queryset = Tracking.objects.all()
+        ser = TrackingSerializer(queryset, many=True)
+
+        return Response(ser.data, status=200)
 
