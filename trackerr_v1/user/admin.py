9cfd56918acfecfc3824aa5f1326a61cf4f480a2
iamukn <n.u.kingsley@gmail.com>
2024-02-14 20:19:33 +0000

    added the models to the admin panel

--- trackerr_v1/user/admin.py
+++ trackerr_v1/user/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import (User, Logistics_partner, Business_owner)
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Logistics_partner)
+admin.site.register(Business_owner)
