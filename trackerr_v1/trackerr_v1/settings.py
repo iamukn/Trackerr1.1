226982299dca15817cf9c35102ec5d6ddbdd0bcc
iamukn <n.u.kingsley@gmail.com>
2024-02-14 17:30:52 +0000

    added the cors configuration

--- trackerr_v1/trackerr_v1/settings.py
+++ trackerr_v1/trackerr_v1/settings.py
@@ -37,11 +37,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'rest_framework',
-    'user',
+    "rest_framework",
+    "corsheaders",
+    "user",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -50,6 +53,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS: True
 
 ROOT_URLCONF = "trackerr_v1.urls"
 
