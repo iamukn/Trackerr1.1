c7e5aaa97f7f4a1f86861b27c8fef9a4ae928f76
iamukn <n.u.kingsley@gmail.com>
2024-02-18 18:56:43 +0000

    added a view and urls path to business

--- trackerr_v1/trackerr_v1/urls.py
+++ trackerr_v1/trackerr_v1/urls.py
@@ -24,5 +24,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('user.urls')),
+    path('', include('business.urls')),
     ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
