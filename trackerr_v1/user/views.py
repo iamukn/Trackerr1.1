9f5f8e033b869ed4ea8119e4577cb03cf497eaab
iamukn <n.u.kingsley@gmail.com>
2024-02-20 20:39:56 +0000

    updated the users view

--- trackerr_v1/user/views.py
+++ trackerr_v1/user/views.py
@@ -34,30 +34,6 @@ class UsersView(APIView):
 
         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    def post(self, request, *args, **kwargs):
-        try:
-
-            user_serializer = UsersSerializer(data=request.data)
-            
-            if user_serializer.is_valid():
-                user_serializer.save()
-
-        except Exception:
-            raise ValueError
-
-        if request.data.get('account_type') == 'business':            
-            user = User.objects.get(email=request.data.get('email'))
-            business_serializer = Business_ownerSerializer(data={'business_name': request.data.get('business_name')})
-            if business_serializer.is_valid():
-                business_serializer.save(user=user)
-                return Response(business_serializer.data, status=status.HTTP_201_CREATED)
-            return Response(business_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            
-        elif request.data.get('account_type') == 'logistics':
-            pass
-
-        else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 class UserView(UsersView):
     """ Endpoint to handle individual user data update 
@@ -84,4 +60,3 @@ class UserView(UsersView):
 
             return Response(serializer.data, status.HTTP_206_PARTIAL_CONTENT)
         return Response(status.HTTP_404_NOT_FOUND)
-
