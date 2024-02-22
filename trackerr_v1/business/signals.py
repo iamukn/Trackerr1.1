bf39dd0550c0080098bfc9829bb98206e3605905
iamukn <n.u.kingsley@gmail.com>
2024-02-22 20:16:02 +0000

    changed the model from User to Business_owner on the business signal

--- trackerr_v1/business/signals.py
+++ trackerr_v1/business/signals.py
@@ -2,7 +2,7 @@
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from user.models import User
+from business.models import Business_owner
 from django.core.mail import send_mail
 from django.conf import settings
 
@@ -10,15 +10,15 @@ from django.conf import settings
     Signals to send emails as soon as a User is Created
 """
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=Business_owner)
 def send_welcome_email(sender, instance, created, **Kwargs):
 
     if created:# only send email for new users
 
         subject = 'Welcome to Trackerr!!'
-        message = f'Hello {instance.name}, you account has been successfully regisitered and you are set to start tracking your parcels in realtime ;)'
+        message = f'Hello {instance.user.name}, you account has been successfully regisitered and you are set to start tracking your parcels in realtime ;)'
         from_email = settings.EMAIL_HOST_USER 
-        to = [instance.email,]
+        to = [instance.user.email,]
         try:
             send_mail(subject, message, from_email, to)
         except Exception:
