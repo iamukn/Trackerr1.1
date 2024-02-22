396037933803bdad5d09dc4b9543e2642ca0c26f
iamukn <n.u.kingsley@gmail.com>
2024-02-22 19:22:47 +0000

    email messaging functionality added for registered users

---
+++ trackerr_v1/business/signals.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from user.models import User
+from django.core.mail import send_mail
+from django.conf import settings
+
+""" 
+    Signals to send emails as soon as a User is Created
+"""
+
+@receiver(post_save, sender=User)
+def send_welcome_email(sender, instance, created, **Kwargs):
+
+    if created:# only send email for new users
+
+        subject = 'Welcome to Trackerr!!'
+        message = f'Hello {instance.name}, you account has been successfully regisitered and you are set to start tracking your parcels in realtime ;)'
+        from_email = settings.EMAIL_HOST_USER 
+        to = [instance.email,]
+        try:
+            send_mail(subject, message, from_email, to)
+        except Exception:
+        # Write a logging for this incase an exception occurs
+            print('ERROR IN BUSINESS SIGNALS, Please check!!')
--- trackerr_v1/trackerr_v1/settings.py
+++ trackerr_v1/trackerr_v1/settings.py
@@ -154,6 +154,12 @@ STATIC_URL = "static/"
 MEDIA_ROOT = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env('EMAIL')
+EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
