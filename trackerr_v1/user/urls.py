6c5458021bc88cf5d1c619eaa2c8eadfd5478e79
iamukn <n.u.kingsley@gmail.com>
2024-03-05 14:08:58 +0000

    added the change password functionality

--- trackerr_v1/user/urls.py
+++ trackerr_v1/user/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import views, users_count, password_recovery
+from .views import (
+    views,
+    users_count,
+    password_recovery,
+    change_password
+)
 
 
 urlpatterns = [
@@ -7,4 +12,5 @@ urlpatterns = [
     path('user/<int:pk>/', views.UserView.as_view(), name='user'),
     path('users-count/',users_count.Users_count.as_view(), name='users-count'),
     path('reset-password/',password_recovery.Recover_password.as_view(), name='recovery'),
+    path('change-password/', change_password.ChangePassword.as_view(), name='change-password'),
         ]
---
+++ trackerr_v1/user/views/change_password.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from .password_recovery import Recover_password
+from django.shortcuts import get_object_or_404
+from user.models import User
+
+""" Change Password Feature """
+
+class ChangePassword(Recover_password):
+    ''' Route for password change for authenticated users '''
+    permission_classes = [IsAuthenticated,]
+
+    def post(self, request, *args, **kwargs):
+        # Receives a post request from Logged in user for password change
+        email = request.user.email
+        user = self.get_queryset(email)
+        password1 = request.data.get('password1')
+        password2 = request.data.get('password2')
+       
+        if user and password1 == password2:
+            user.set_password(password1)
+            user.save()
+            return Response(status=status.HTTP_206_PARTIAL_CONTENT)
+        
+        return Response(status=status.HTTP_403_FORBIDDEN)
