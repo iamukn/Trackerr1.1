05ae13856d0b949a412a16e4ab1b9e3b3bdba810
iamukn <n.u.kingsley@gmail.com>
2024-03-07 19:52:38 +0000

    added a permission class to the business count route

--- trackerr_v1/business/views/business_owners_count.py
+++ trackerr_v1/business/views/business_owners_count.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAdminUser
 from rest_framework.views import APIView
 from business.models import Business_owner
 
@@ -7,7 +8,7 @@ from business.models import Business_owner
 
 
 class Business_count(APIView):
-
+    permission_classes = [IsAdminUser,]
     def get(self, request, *args, **kwargs):
         # Returns the count of only the business owners
         counts = Business_owner.objects.all().count()
