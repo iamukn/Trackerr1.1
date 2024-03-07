c2fff4f167d2fd562a85e6eea32dacf3e44cea00
iamukn <n.u.kingsley@gmail.com>
2024-03-07 20:15:40 +0000

    added a permission to the logistic count route

--- trackerr_v1/logistics/views/logistics_users_count.py
+++ trackerr_v1/logistics/views/logistics_users_count.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAdminUser
 from rest_framework.views import APIView
 from logistics.models import Logistics_partner
 
@@ -7,7 +8,7 @@ from logistics.models import Logistics_partner
 
 
 class Business_count(APIView):
-
+    permission_classes = [IsAdminUser,]
     def get(self, request, *args, **kwargs):
         # Returns the count of only the business owners
         counts = Logistics_partner.objects.all().count()
