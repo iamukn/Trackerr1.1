21f0ffa883c14dacce470f39d0401b6a79201a02
iamukn <n.u.kingsley@gmail.com>
2024-03-09 20:27:49 +0000

    added the tracking model to the db

--- trackerr_v1/tracking/models.py
+++ trackerr_v1/tracking/models.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python3
+from datetime import date
 from django.db import models
 
-# Create your models here.
+""" Tracking model """
+
+class Tracking(models.Model):
+    parcel_number = models.CharField(max_length=15, null=False, blank=False)
+    date_of_purchase = models.DateField(default=date.today, null=False, blank=False)
+    delivery_date = models.DateField(default=date.today, null=False, blank=False) 
+    shipping_address = models.CharField(max_length=255, null=False, blank=False)
+    vendor = models.CharField(max_length=255, null=False, blank=False)
+    status = models.CharField(max_length=15, null=False, blank=False, default="Pending")
+
+    class Meta:
+        verbose_name = 'tracking'
