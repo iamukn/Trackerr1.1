2dd152dc93f7c5514b51c4c45ed765dad3f6bb60
iamukn <n.u.kingsley@gmail.com>
2024-03-11 20:07:02 +0000

    updated the track_gen file

--- trackerr_v1/tracking_information/utils/track_gen.py
+++ trackerr_v1/tracking_information/utils/track_gen.py
@@ -19,5 +19,5 @@ def tracking_number_gen(user: str) -> str:
         user = user.split(' ')
         tracking_num = '{0}{1}{2}'.format(user[0][0:2].upper(), rand_num, user[1][-2:].upper())
         return tracking_num
-    tracking_num = '{0}{1}{2}'.format(user[0:2], rand_num, user[-2:-1])
+    tracking_num = '{0}{1}{2}'.format(user[0:2], rand_num, user[-2:])
     return tracking_num
