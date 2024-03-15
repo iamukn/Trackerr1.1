7de33c4aa0c1975f161724a874b997cde793cf1f
iamukn <n.u.kingsley@gmail.com>
2024-03-15 12:35:36 +0000

    test that ensures that only a business owner can generate a tracking number

--- trackerr_v1/trackerr_v1/urls.py
+++ trackerr_v1/trackerr_v1/urls.py
@@ -37,4 +37,3 @@ urlpatterns = [
     path('api/token/', TokenObtain.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
