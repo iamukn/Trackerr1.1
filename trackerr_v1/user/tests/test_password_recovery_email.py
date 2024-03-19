0e22dfc885e50c62ac36f560186424b7c358e57e
iamukn <n.u.kingsley@gmail.com>
2024-03-19 12:24:54 +0000

    added a test that ensures that email recovery messages are sent successfully

---
+++ trackerr_v1/user/tests/test_password_recovery_email.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from rest_framework_simplejwt.tokens import AccessToken
+from unittest.mock import patch, ANY
+from user.models import User
+from user.views.password_recovery import Recover_password
+from authentication.test_email import emailer
+from business.models import Business_owner
+
+""" Test the email messaging endpoint for password recovery """
+
+
+class TestPasswordRecoveryEmailandChange(APITestCase):
+    """  password reset test """
+
+    def setUp(self):
+        self.user = User.objects.create(
+            name = 'Jane Doe',
+            email = 'officialtrackerr@gmail.com',
+            phone_number='09023456789',
+            address='Ikeja, Lagos',
+            password='password',
+            account_type='business'
+                )
+
+        self.token = AccessToken.for_user(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer %s"%self.token)
+        self.business = Business_owner.objects.create(user=self.user, business_name='haplotype')
+    
+    @patch('user.views.password_recovery.Thread')
+    def test_can_send_recovery_email(self, mock_thread):
+        url = reverse('recover-password')
+        res = self.client.post(url, data={'email': self.user.email})
+        self.assertTrue(res.status_code == 200)
+        # mocks the thread in charge of sending recovery emails with a one time token
+        mock_thread.assert_called_once_with(target=emailer, kwargs={'subject': 'Password reset', 'to': self.user.email, 'contents': ANY}, daemon=True)
+
+    def test_change_password(self):
+        pass
