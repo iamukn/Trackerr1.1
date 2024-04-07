3e1fc3e5863a1325e5962f09fb8f58dfa5f8d946
iamukn <n.u.kingsley@gmail.com>
2024-04-07 22:27:07 +0000

    updated the endpoint to the status count

--- trackerr_v1/tracking_information/urls.py
+++ trackerr_v1/tracking_information/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('generate-tracking/', generate_tracking_view.GenerateView.as_view(), name='generate-tracking'),
     path('tracking/<str:num>/', retrieve_one.RetrieveOne.as_view(), name='track-one'),
     path('trackings/', retrieve_all_tracking.RetrieveAllView.as_view(), name='trackings'),
-    path('status-count/', retrieve_status_count.RetrieveStatusCount.as_view(), name='status-count'),
+    path('trackings/status-count/', retrieve_status_count.RetrieveStatusCount.as_view(), name='status-count'),
         ]
