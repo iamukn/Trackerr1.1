9b415048deb5c0f7f3ae1a121f1784664ccb4758
iamukn <n.u.kingsley@gmail.com>
2024-04-25 22:20:17 +0100

    updated the generate tracking test

--- trackerr_v1/tracking_information/tests/test_tracking_generation.py
+++ trackerr_v1/tracking_information/tests/test_tracking_generation.py
@@ -4,6 +4,7 @@
 from business.models import Business_owner
 from django.urls import reverse
 from rest_framework import status
+from unittest.mock import patch, MagicMock
 from rest_framework.utils.serializer_helpers import ReturnDict
 from rest_framework.test import APITestCase
 from rest_framework_simplejwt.tokens import AccessToken
@@ -22,20 +23,30 @@ class TestTrackingGenerationEndpoint(APITestCase):
                 password='password',
                 account_type='business'
                 )
-        self.data = {'product': 'Medicine','shipping_address':'Lagos, Ibadan', 'country': 'Nigeria', 'quantity': 2, 'delivery_date': '2024-12-12'}
+        self.data = {'product': 'Medicine','shipping_address':'Authority avenue ikotun lagos', 'country': 'Nigeria', 'quantity': 2, 'delivery_date': '2024-12-12'}
         self.token = AccessToken.for_user(user=self.user)
         self.client.credentials(HTTP_AUTHORIZATION="Bearer %s"%self.token)
         self.business = Business_owner.objects.create(user=self.user, business_name='Hue Logistics')
-"""
-    def test_tracking_generation_by_business_owner(self):
+        self.return_value = {'address': 'Authority Ave, Alimosho, Nigeria', 'city': 'Lagos', 'country': 'Nigeria', 'latitude': 6.54219, 'longitude': 3.22122}
+        self.track_num = 'J123456778OE'
+
+    @patch('tracking_information.utils.tracking_class.Track_gen')
+    @patch('tracking_information.utils.validate_shipping_address.verify_address')
+    def test_tracking_generation_by_business_owner(self, mock_track_gen, mock_verify_address):
         # test to ensure that business users only creates tracking
         url = reverse('generate-tracking')
-        
+        mock_track_gen_instance = MagicMock()
+        mock_verify_address_instance = MagicMock()
+
+        mock_track_gen_instance.generate_tracking_number.return_value = self.track_num
+        mock_track_gen.return_value = mock_track_gen_instance
+
+        mock_verify_address.return_value = self.return_value
+
         res = self.client.post(url, data=self.data, format='json')
-        
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         self.assertTrue(type(res.data), ReturnDict)
-
+"""
     def test_raises_a_404_for_missing_required_fields(self):
         # test to ensure that a 404 is raised if required fields aren't provided
         url = reverse('generate-tracking')
