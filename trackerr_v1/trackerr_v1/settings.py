384623c19eec0814952d1fac28b77023a6e9c72f
iamukn <n.u.kingsley@gmail.com>
2024-05-18 09:43:05 +0100

    updated the settings.py to get the environment file from the host machine

--- trackerr_v1/trackerr_v1/settings.py
+++ trackerr_v1/trackerr_v1/settings.py
@@ -13,18 +13,20 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import environ
 from datetime import timedelta
+import os
 
 
-env = environ.Env(
-    DEBUG=(bool, False)
-        )
+#env = environ.Env(
+#    DEBUG=(bool, False)
+#        )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-environ.Env.read_env(BASE_DIR / '.env')
+#environ.Env.read_env(BASE_DIR / '.env')
 
+env = os.environ.get
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
