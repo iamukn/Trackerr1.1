b23a2643b1ada871709ea6d7a3379107a7f65332
iamukn <n.u.kingsley@gmail.com>
2024-07-01 01:09:55 +0100

    updated the shipping address verification function to use background process

---
+++ trackerr_v1/shared/celery_tasks/tracking_info_tasks/verify_address_task.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from celery import shared_task
+from django.conf import settings
+from tracking_information.utils.validate_shipping_address import verify_address
+""" Tasks that sends recovery email """
+
+@shared_task(bind=True, name='verify shipping address')
+
+def verify_shipping_address(self, address:str):
+    addr = verify_address(address=address)
+    return addr
--- trackerr_v1/tracking_information/views/generate_tracking_view.py
+++ trackerr_v1/tracking_information/views/generate_tracking_view.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from tracking_information.utils.tracking_class import Track_gen
 from tracking_information.serializer import Tracking_infoSerializer
 from tracking_information.models import Tracking_info
+from shared.celery_tasks.tracking_info_tasks.verify_address_task import verify_shipping_address
 from tracking_information.utils.validate_shipping_address import verify_address
 from rest_framework.permissions import IsAuthenticated 
 from business.views.business_owner_permission import IsBusinessOwner
@@ -26,7 +27,9 @@ class GenerateView(APIView):
     # method that handles the POST request
     def post(self, request, *args, **kwargs):
         try:
-            address = verify_address(request.data.get('shipping_address'))
+            #address = verify_address(request.data.get('shipping_address'))
+            # retrieve the location data using celery
+            address = verify_shipping_address.apply_async(kwargs={'address': request.data.get('shipping_address')}).get()
             # retrieves all the data from the requuest, generate a tracking number and return to user
             data = {
                 "shipping_address": address.get('address'),
