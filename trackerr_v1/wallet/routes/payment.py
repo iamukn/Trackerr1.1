d352a3bdea371de02faf6b95419ed139bd341f54
iamukn <n.u.kingsley@gmail.com>
2026-04-06 23:57:00 +0000

    added a payment initialization route

--- trackerr_v1/wallet/routes/payment.py
+++ trackerr_v1/wallet/routes/payment.py
@@ -2,7 +2,30 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from business.views.business_owner_permission import IsBusinessOwner
+from wallet.payment_logic.payment_logic import initialize_payment
 
+
+
+class PaymentDeposit(APIView):
+    """
+      Handles Deposit for business owners
+    """
+    permission_classes = [AllowAny,]
+
+
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        amount = request.data.get('amount')
+
+        try:
+            payment_initialized = initialize_payment(email, amount, 'NGN')
+
+            # Add payment info to DB USING Transactions
+            #add_payment_info_to_db(payment_initialized, email, amount, 'NGN')
+            return Response({'msg': 'success', 'autorization_url': payment_initialized.get('data').get('authorization_url')}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'msg': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 class PaymentWebhook(APIView):
     permission_classes = [AllowAny,]
