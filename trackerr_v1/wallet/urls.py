c0de81c10978de17283813ce1e0d2636016befdf
iamukn <n.u.kingsley@gmail.com>
2026-04-06 23:58:51 +0000

    updated the path

--- trackerr_v1/wallet/urls.py
+++ trackerr_v1/wallet/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from wallet.routes.payment import PaymentWebhook
+from wallet.routes.payment import PaymentWebhook, PaymentDeposit
 
 urlpatterns = [
+    path('initialize', PaymentDeposit.as_view(), name='initialize-deposit'),
     path('webhook', PaymentWebhook.as_view(), name='payment-webhook')
         ]
